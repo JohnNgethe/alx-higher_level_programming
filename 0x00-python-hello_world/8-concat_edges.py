0a257e399f7907dd881becdd44336fba3102f1e4
John Ngethe <jngethe07@gmail.com>
2022-11-28 14:28:52 -0800

    changes 6

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f"{str[39:67]} {str[107:112]} {str[:6]}"
-print(f"{str}\n")
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
