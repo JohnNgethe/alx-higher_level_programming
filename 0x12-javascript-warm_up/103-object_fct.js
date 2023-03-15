ffba03c61199145c0bfab78b113a782d674826d6
John Ngethe <jngethe07@gmail.com>
2023-03-15 09:50:21 -0700

    103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () { myObject.value += 1; };
+
+myObject.incr = () => myObject.value++;
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
